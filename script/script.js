6a52a3c78d34440b4caefb46d3d7491cc4c9702d
sample <youngjae7981@gmail.com>
2023-02-06 11:07:56 +0900

    dasdasda

---
+++ script/script.js
@@ -0,0 +1,12 @@
+// for (i = 0; i < 10; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+// 피보나치 수열 : 1 1 2 3 5 8 13 21 34 55
+var a = 1;
+var b = 1;
+for (i = 0; i < 5; i++) {
+  var result = a + b;
+  console.log(result);
+}
